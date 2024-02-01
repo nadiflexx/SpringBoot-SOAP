3bb9ed9d9429e530ce262fbe5b5de55b43030f92
Nadiflexx <nadi-1999@hotmail.es>
2024-02-01 21:16:48 +0100

    [#1] Generated XML Classes automatically

---
+++ src/test/java/org/example/springbootsoap/SpringBootSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
